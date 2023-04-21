62b1628f61d8b305410675487b90199ab2888144
Romanin <60302782+romanin-rf@users.noreply.github.com>
2023-04-21 21:54:00 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,12 +1,2 @@
 @echo off
-python builder.py -pv cp39 -st win_amd64 -pdp "{localdir}{sep}pciw{sep}data{sep}t_cpu{sep}Windows-x86_64,{localdir}{sep}pciw{sep}data{sep}nsmi{sep}Windows-x86_64"
-python builder.py -pv cp310 -st win_amd64 -pdp "{localdir}{sep}pciw{sep}data{sep}t_cpu{sep}Windows-x86_64,{localdir}{sep}pciw{sep}data{sep}nsmi{sep}Windows-x86_64"
-python builder.py -pv py39 -st win_amd64 -pdp "{localdir}{sep}pciw{sep}data{sep}t_cpu{sep}Windows-x86_64,{localdir}{sep}pciw{sep}data{sep}nsmi{sep}Windows-x86_64"
-python builder.py -pv py310 -st win_amd64 -pdp "{localdir}{sep}pciw{sep}data{sep}t_cpu{sep}Windows-x86_64,{localdir}{sep}pciw{sep}data{sep}nsmi{sep}Windows-x86_64"
-
-python builder.py -pv cp39 -st manylinux1_x86_64 -pdp "{localdir}{sep}pciw{sep}data{sep}nsmi{sep}Linux-x86_64"
-python builder.py -pv cp310 -st manylinux1_x86_64 -pdp "{localdir}{sep}pciw{sep}data{sep}nsmi{sep}Linux-x86_64"
-python builder.py -pv py39 -st manylinux1_x86_64 -pdp "{localdir}{sep}pciw{sep}data{sep}nsmi{sep}Linux-x86_64"
-python builder.py -pv py310 -st manylinux1_x86_64 -pdp "{localdir}{sep}pciw{sep}data{sep}nsmi{sep}Linux-x86_64"
-
-python setup.py sdist
+python builder.py
